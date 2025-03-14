fa18a93406608b24838b821866acf9147f5287ab
Chris Overcash <covercash2@gmail.com>
2025-03-14 18:15:13 -0500

    chore: save game

---
+++ export-esp.sh
@@ -0,0 +1,2 @@
+export LIBCLANG_PATH="/Users/chrash/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
+export PATH="/Users/chrash/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin:$PATH"
