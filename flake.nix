94f27d2ebcb0556609e6ac05a9b3b0a3e1db49ed
Chris Overcash <covercash2@gmail.com>
2025-04-20 11:14:19 -0500

    feat: starting over!

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A minimal Rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+
+            # https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html#for-linux-users
+            bison
+            ccache
+            cmake
+            dfu-util
+            flex
+            git
+            gnumake
+            gperf
+            libusb1
+            ninja
+
+            ldproxy
+            # Add other dependencies here if needed
+          ];
+        };
+      }
+    );
+}
